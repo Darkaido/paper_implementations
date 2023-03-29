7070c39fe51fcbe668c95b13be8640909a16b505
Aman Jain <56019496+Darkaido@users.noreply.github.com>
2023-03-29 23:19:20 +0530

    Basic CNN using pytorch

---
+++ cnn.py
@@ -0,0 +1,120 @@
+# Imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+# create fully connected Network
+class NN(nn.Module):
+    def __init__(self,input_size , num_classes):
+        super(NN,self).__init__()
+        self.fc1 = nn.Linear(input_size,50)
+        self.fc2 = nn.Linear(50,num_classes)
+
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x= self.fc2(x)
+        return x
+
+
+# CNN
+class CNN(nn.Module):
+    def __init__(self,in_channels = 1, num_classes = 10):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8,kernel_size=(3,3),stride=(1,1),padding=(1,1))
+        self.pool1 = nn.MaxPool2d(kernel_size=(2,2),stride = (2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16,kernel_size=(3,3),stride=(1,1),padding=(1,1))
+        self.fc1 = nn.Linear(16*7*7,num_classes)
+
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool1(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+    
+
+
+
+# set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters
+input_size = 784
+num_classes =10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 5
+
+#Load Data
+train_datasets = datasets.MNIST(root='dataset/',train =True,transform=transforms.ToTensor(), download = True)
+train_loader = DataLoader(dataset=train_datasets, batch_size =batch_size,shuffle=True)
+test_datasets = datasets.MNIST(root='dataset/',train =False,transform=transforms.ToTensor(), download = True)
+test_loader = DataLoader(dataset= test_datasets,batch_size = batch_size, shuffle=True)
+
+# Initialize Network
+model = CNN().to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(),lr = learning_rate)
+
+# Train Network
+
+for epoch in range(num_epochs):
+    print(f"Epoch: {epoch}")
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # Get data to cuda if possible
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+
+        # Get to correct shape, 28x28->784
+        # -1 will flatten all outer dimensions into one
+        # data = data.reshape(data.shape[0], -1) 
+
+        # forward propagation
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # zero previous gradients
+        optimizer.zero_grad()
+        
+        # back-propagation
+        loss.backward()
+
+        # gradient descent or adam step
+        optimizer.step()
+
+# check accuracy on training & test to see how good our model
+
+def check_accuracy(loader,model):
+    if loader.dataset.train:
+        print("Checking accuracy on training data")
+    else:
+        print("checking accuracy on test data")
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x,y in loader:
+            x = x.to(device =device)
+            y = y.to(device= device)
+            # x = x.reshape(x.shape[0],-1)
+
+            scores = model(x)
+            _,predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)* 100:.2f}')
+    model.train()
+
+check_accuracy(train_loader,model)
+check_accuracy(test_loader,model)
